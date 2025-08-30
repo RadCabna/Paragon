6c0e81c3ef2c20cc48ab4a8b1eab348df2c709f9
Radcabna <radcabna@gmail.com>
2025-08-30 14:00:16 +0300

    Initial Commit

---
+++ Paragon_Game/Paragon_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Paragon_GameApp.swift
+//  Paragon_Game
+//
+//  Created by Алкександр Степанов on 30.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Paragon_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
